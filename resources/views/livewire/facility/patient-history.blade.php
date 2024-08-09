3ee7f6c3fa30b7a3b5220f3748dca0b7b511f04a
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-08-09 08:50:31 +0800

    test

--- resources/views/livewire/facility/patient-history.blade.php
+++ resources/views/livewire/facility/patient-history.blade.php
@@ -40,6 +40,7 @@
               <tr class="divide-x divide-gray-200">
                 <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">Facility</th>
                 <th scope="col" class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
+                <th scope="col" class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Case</th>
                 <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-0">Diagnosis</th>
               </tr>
             </thead>
@@ -49,6 +50,7 @@
                 <td class="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">{{$item->facility->name}}</td>
                 <td class="whitespace-nowrap p-4 text-sm text-gray-500">{{Carbon\Carbon::parse($item->created_at)->format('F, d Y')}}</td>
                 <td class="whitespace-nowrap p-4 text-sm text-gray-500">{{$item->diagnosis}}
+                <td class="whitespace-nowrap p-4 text-sm text-gray-500">{{$item->patient->caseCategory->name}}
                 </td>
               </tr>
             @empty
