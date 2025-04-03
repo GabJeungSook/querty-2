2ad4f69fcce8f75af6fc14f284b34b34a2a5a9ec
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-04-03 16:00:43 +0800

    changes

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -178,7 +178,7 @@
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-green-200 px-6">
         <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=800" alt="Your Company">
+          {{-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=800" alt="Your Company"> --}}
           <span class="px-2 signika-negative-300 text-4xl text-gray-700">QUERTY</span>
         </div>
         <nav class="flex flex-1 flex-col">
